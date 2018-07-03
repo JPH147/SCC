bd2d6abeca4503e3e50301a9f7c8761459bbbd98
Jean Pierre <jph.rodriguezf@gmail.com>
2018-07-03 10:00:21 -0500

    Mejora en el menú

--- FrontEnd/src/app/components/usuarios/usuarios.component.ts
+++ FrontEnd/src/app/components/usuarios/usuarios.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosComponent implements OnInit {
 
+  public usuarios: Array<usuario>;
+
   constructor() { }
 
   ngOnInit() {
+  	this.usuarios=[
+  	{nombre:"Jean Pierre Rodriguez Farfan", rol:"Administrador", cargo:"Tercero", ultimo_login:"03/08/2018", email:"jeanpierre.rodriguez@genussolucionesti.com", telefono:"996040111"}
+  	]
   }
 
+}
+
+export interface usuario{
+	nombre:string;
+	rol: string;
+	cargo:string;
+	ultimo_login:string;
+	email:string;
+	telefono:string;
 }
