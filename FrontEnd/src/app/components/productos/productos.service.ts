b2b6717b4336c904aea27b47faab2c546b2c8c7e
Jean Pierre <jph.rodriguezf@gmail.com>
2018-07-10 16:24:49 -0500

    Prueba con servicios

--- FrontEnd/src/app/components/productos/productos.service.ts
+++ FrontEnd/src/app/components/productos/productos.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators';
-import {URL} from '../../global/url';
 
 @Injectable()
 
