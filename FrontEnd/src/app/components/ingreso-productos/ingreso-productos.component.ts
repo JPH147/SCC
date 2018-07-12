f0833e9b081b8579c83740b7fcda6587a1dfffbf
Carlos <chrodriguez82@gmail.com>
2018-07-12 08:51:09 -0500

    Modificaciones Productos

--- FrontEnd/src/app/components/ingreso-productos/ingreso-productos.component.ts
+++ FrontEnd/src/app/components/ingreso-productos/ingreso-productos.component.ts
@@ -4,6 +4,11 @@ import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 import {SelectionModel} from '@angular/cdk/collections';
 import {MatTableDataSource} from '@angular/material';
+
+export interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-ingreso-productos',
   templateUrl: './ingreso-productos.component.html',
@@ -18,6 +23,11 @@ import {MatTableDataSource} from '@angular/material';
     displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
     dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
     selection = new SelectionModel<PeriodicElement>(true, []);
+    foods: Food[] = [
+      {value: 'steak-0', viewValue: 'Almacen Principal'},
+      {value: 'pizza-1', viewValue: 'Almacen Dos'},
+      {value: 'tacos-2', viewValue: 'Almacen Tres'}
+    ];
 
     constructor() { }
 
@@ -53,6 +63,7 @@ import {MatTableDataSource} from '@angular/material';
     weight: number;
     symbol: string;
   }
+
 
   const ELEMENT_DATA: PeriodicElement[] = [
     {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
