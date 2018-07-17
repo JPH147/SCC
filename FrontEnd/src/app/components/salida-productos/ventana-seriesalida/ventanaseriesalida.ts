af5ab1c54f3b71c4e3da683b4ea4163d0453b661
Carlos <chrodriguez82@gmail.com>
2018-07-17 17:23:23 -0500

    Salida Productos

---
+++ FrontEnd/src/app/components/salida-productos/ventana-seriesalida/ventanaseriesalida.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import { MatCard, MatInputModule, MatButton, MatDatepicker, MatTableModule } from '@angular/material';
+
+
+@Component({
+  selector: 'app-ventanaseriesalida',
+  templateUrl: './ventanaseriesalida.html',
+  styleUrls: ['./ventanaseriesalida.css']
+})
+
+// tslint:disable-next-line:class-name
+export class ventanaseriesalida  implements OnInit {
+  public seriearticulo: Array<any> ;
+  public numero: number;
+  public series: Array <string>;
+  public contador: number;
+  public serie: Array<any>;
+
+
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+      this.contador = 1;
+      this.seriearticulo = [{numero: this.contador, series: ''} ];
+    }
+
+
+
+  AgregarSerieVSalida() {
+    this.contador++;
+    this.seriearticulo.push({numero: this.contador, series: ''});
+  }
+
+  Aceptar() {
+    console.log(this.seriearticulo);
+  }
+}
