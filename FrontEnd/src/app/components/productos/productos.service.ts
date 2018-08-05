f682cb524374c0f4df9d7e0725d186a046e9050b
Jean Pierre Rodriguez <jph.rodriguezf@gmail.com>
2018-08-04 21:25:07 -0500

    update component

--- FrontEnd/src/app/components/productos/productos.service.ts
+++ FrontEnd/src/app/components/productos/productos.service.ts
@@ -16,12 +16,6 @@ export class ProductoService {
   tipo: string,
   marca: string,
   modelo: string,
-<<<<<<< HEAD
-  descripcion: string
-  ): Observable<Producto[]>
-  { return this.http.get(this.url + 'producto/read.php?prtipo='
-   + tipo + '&prmarca=' + marca + '&prmodelo=' + modelo + '&prdescripcion='+ descripcion)
-=======
   descripcion: string,
   pagina: number,
   total_pagina: number,
@@ -40,7 +34,6 @@ export class ProductoService {
       .set('prtotalpagina', total_pagina.toString())
       .set('orden', orden)
   })
->>>>>>> c9be6c288668a3311e92483fddb43a8fe3aa78b1
     .pipe(map(res => {
       if (res['codigo'] === 0) {
           return res;
@@ -104,17 +97,6 @@ export class ProductoService {
 }
 
 export interface Producto {
-<<<<<<< HEAD
-  numero: number,
-  id: number,
-  nombre: string,
-  tipo: string,
-  marca: string,
-  modelo: string,
-  descripcion: string,
-  unidad_medida: string,
-  precio:number
-=======
   numero: number;
   id: number;
   nombre: string;
@@ -124,5 +106,4 @@ export interface Producto {
   descripcion: string;
   unidad_medida: string;
   precio: number;
->>>>>>> c9be6c288668a3311e92483fddb43a8fe3aa78b1
 }
