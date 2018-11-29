d59fb36608fa06a08d2ec9092584acc60ab3b6ff
JeanPierreRodriguez <jph.rodriguezf@gmail.com>
2018-11-29 11:32:46 -0500

    Bug

--- FrontEnd/src/app/components/evaluacion/evaluacion-cuotas/evaluacion-cuotas.component.ts
+++ FrontEnd/src/app/components/evaluacion/evaluacion-cuotas/evaluacion-cuotas.component.ts
@@ -9,9 +9,47 @@ import {CollectionViewer} from '@angular/cdk/collections';
 })
 export class EvaluacionCuotasComponent implements OnInit {
 
+  public interes:number;
+  public aporte:number;
+  public capacidad:number;
+  public capital: number;
+  public cuotas: number;
+  public prestamo: number;
+  public fecha_inicio:Date;
+  public cronograma:Array<Cronograma>=[];
+
   constructor() { }
 
   ngOnInit() {
+    this.cuotas=12;
+    this.interes=0.15;
+    this.aporte=20;
+    this.capacidad=528;
+    this.fecha_inicio=new Date();
+    // this.CalcularPrimerPago()
+  }
+
+  CalcularPrimerPago(){
+    let fecha:Date;
+    for(let i=1; i=12;i++){
+      fecha=new Date(this.fecha_inicio.valueOf() + 1000*60*60*24*30*i);
+      this.cronograma.push({
+        numero:i,
+        monto:this.capacidad-this.aporte,
+        aporte:this.aporte,
+        fecha:fecha,
+        total:this.capacidad
+      })
+    }
+    this.CalcularPrimerosTotales()
+  }
+
+  CalcularPrimerosTotales(){
+    this.prestamo=0;
+    this.cronograma.forEach((item)=>{
+      this.prestamo+=item.monto;
+    })
+    this.capital=this.prestamo/(this.cuotas*this.interes)
   }
 
 }
@@ -28,4 +66,12 @@ export class EvaluacionCoutasDataSource {
     this.Cuotas.complete();
   }
   
+}
+
+export interface Cronograma{
+  numero:number,
+  monto:number,
+  aporte:number,
+  fecha: Date,
+  total:number
 }
--- FrontEnd/src/app/components/evaluacion/evaluacion.component.ts
+++ FrontEnd/src/app/components/evaluacion/evaluacion.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ViewChild, ViewChildren, ElementRef, QueryList, AfterViewInit } from '@angular/core';
-import {fromEvent, merge} from 'rxjs';
-import {tap, debounceTime, distinctUntilChanged} from'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
@@ -10,18 +8,6 @@ import {tap, debounceTime, distinctUntilChanged} from'rxjs/operators';
 })
 export class EvaluacionComponent implements OnInit {
 
-  public sueldo_bruto: number;
-  public total_descuento_oficial: number;
-  public sueldo_base: number;
-  public sueldo_disponible:number;
-  public total_descuento_personal:number;
-  public capacidad_pago:number;
-  public descuento_oficial: Array<any>=[];
-  public descuento_personal:Array<any>=[];
-
-  @ViewChild('InputSueldo') FiltroSueldo: ElementRef;
-  @ViewChildren('InputDescuento') FiltroDescuento: QueryList<any>;
-
   constructor() { }
 
   ngOnInit() {
@@ -29,61 +15,7 @@ export class EvaluacionComponent implements OnInit {
   }
 
   ngAfterViewInit(){
-    fromEvent(this.FiltroSueldo.nativeElement,'keyup')
-    .pipe(
-      debounceTime(200),
-      distinctUntilChanged(),
-      tap(()=>{
-        this.CalcularTotales()
-      })
-    ).subscribe()
-
-  this.FiltroDescuento.changes.subscribe(res=>{
-    for (let i in this.FiltroDescuento['_results']) {
-      fromEvent(this.FiltroDescuento['_results'][i].nativeElement,'keyup')
-      .pipe(
-        debounceTime(200),
-        distinctUntilChanged(),
-        tap(()=>{
-          this.CalcularTotales()
-        })
-      ).subscribe()
-    }
-  })
-
   }
 
-  AgregarDescuentoOficial(){
-    this.descuento_oficial.push({descuento:null})
-  }
-
-  AgregarDescuentoPersonal(){
-    this.descuento_personal.push({descuento:null})
-  }
-  
-  CalcularDescuentoTotal(){
-    this.total_descuento_oficial=0;
-    this.total_descuento_personal=0;
-    this.descuento_oficial.forEach((item)=>{
-      this.total_descuento_oficial=this.total_descuento_oficial+item.descuento*1
-    })
-    this.descuento_personal.forEach((item)=>{
-      this.total_descuento_personal=this.total_descuento_personal+item.descuento*1
-    })
-  }
-
-  CalcularTotales(){
-
-    this.CalcularDescuentoTotal()
-
-    this.sueldo_base=0;
-    this.sueldo_disponible=0;
-    this.capacidad_pago=0;
-
-    this.sueldo_base=this.sueldo_bruto-this.total_descuento_oficial;
-    this.sueldo_disponible=this.sueldo_base*0.5;
-    this.capacidad_pago=this.sueldo_disponible-this.total_descuento_personal;
-
-  }
 
 }
